b56ed7665ba9f07d81c1bad49cbf963cdd39c5c6
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-03 16:15:48 +0100

    ♻️ dynamic title and image

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './App.module.css';
 import Title from './components/Title/title';
+import SplashImage from './components/Title/SplashImage/SplashImage';
 
 function App(): JSX.Element {
   return (
     <main className={styles.container}>
-      <Title />
+      <Title text="Bergfest." />
       <div>
         <h2 className={styles.explanation}>Welchen Song wünscht du dir?</h2>
         <form className={styles.form}>
@@ -19,9 +20,8 @@ function App(): JSX.Element {
           />
         </form>
       </div>
-      <img
-        className={styles.image}
-        src="https://images.unsplash.com/photo-1507286722556-2d4b0704eeb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
+      <SplashImage
+        url="https://images.unsplash.com/photo-1507286722556-2d4b0704eeb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
         alt="Stop thinking, start drinking"
       />
     </main>
