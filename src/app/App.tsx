70b41d89ae1e1b302315d3886b9d4cbd5c3498cc
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-03 12:16:03 +0100

    💄 change grid

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,11 +6,13 @@ function App(): JSX.Element {
     <main className={styles.container}>
       <div>
         <h1 className={styles.title}>Bergfest.</h1>
-        <h2 className={styles.explanation}>welchen song wünscht du dir?</h2>
+      </div>
+      <div>
+        <h2 className={styles.explanation}>Welchen Song wünscht du dir?</h2>
         <form className={styles.form}>
-          <input type="text" placeholder="name" />
-          <input type="text" placeholder="favorite song" />
-          <input type="text" placeholder="performed by..." />
+          <input type="text" placeholder="mein name" />
+          <input type="text" placeholder="mein lieblingslied" />
+          <input type="text" placeholder="und das ist von..." />
           <input
             className={styles.button}
             type="submit"
