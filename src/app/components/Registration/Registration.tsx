c7487093aed39ab5b915003734e86b572961b769
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-04 14:45:52 +0100

    ♻️ change login form

--- src/app/components/Registration/Registration.tsx
+++ src/app/components/Registration/Registration.tsx
@@ -1,36 +1,83 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Registration.module.css';
 
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+};
+
 function Registration(): JSX.Element {
-  const [name, setName] = useState('');
-  const [lied, setLied] = useState('');
-  const [interpret, setInterpret] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [disable, setDisable] = useState(false);
+  const [selectedUser, setSelectedUser] = useState('');
 
-  console.log(name, lied, interpret);
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    fetch('https://json-server.machens.dev/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+      }),
+    });
+    setDisable(true);
+  }
+
+  function handleFirstNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setLastName(event.target.value);
+  }
+
+  async function handleSelectClick() {
+    const response = await fetch('https://json-server.machens.dev/users');
+    const newUsers = await response.json();
+    setUsers(newUsers);
+  }
+
+  const userOptions = users.map((user) => (
+    <option key={user.id}>
+      {user.firstName} {user.lastName}
+    </option>
+  ));
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <select
+        onClick={handleSelectClick}
+        onChange={(event) => {
+          setSelectedUser(event.target.value);
+          alert('Hi ' + event.target.value);
+        }}
+        value={selectedUser}
+      >
+        <option disabled>Teilnehmer wählen</option>
+        {userOptions}
+      </select>
+      or
       <input
         type="text"
-        placeholder="name"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
+        placeholder="vorname"
+        value={firstName}
+        onChange={handleFirstNameChange}
       />
-
       <input
         type="text"
-        placeholder="mein lieblingslied"
-        value={lied}
-        onChange={(event) => setLied(event.target.value)}
+        placeholder="nachname"
+        value={lastName}
+        onChange={handleLastNameChange}
       />
-
       <input
-        type="text"
-        placeholder="und das ist von..."
-        value={interpret}
-        onChange={(event) => setInterpret(event.target.value)}
-      />
-
-      <input
+        disabled={disable}
         className={styles.submit}
         type="submit"
         value="dance! dance! dance!"
